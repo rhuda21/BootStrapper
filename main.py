af1d919f451b7d43b77689ad2133bb9f4cd3dfb0
r_soft <muhammadrhuda@outlook.com>
2025-01-23 19:23:32 +0000

    w

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Roblox Executors | BootStrapper")
+
+import os
+import sys
+import subprocess
+subprocess.call(['pip', 'install' ,'requests'])
+import urllib.request
+import requests
+import urllib
+from urllib.request import urlopen
+import urllib3
+folder = (os.getenv('LOCALAPPDATA'))
+
+ver = "1.0"
+updatedl = ""
+
